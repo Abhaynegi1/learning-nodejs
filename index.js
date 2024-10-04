04bae0c4f42d098ecf1c1d7f5da3318dde7a8478
Abhaynegi1 <abhayng12804@gmail.com>
2024-10-04 22:21:35 +0530

    learning about urls

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const http = require("http");
 const fs = require("fs");
-
+const url = require("url");
 const myServer = http.createServer((req, res) => {
+    const myurl = url.parse(req.url , true);
+    console.log(myurl);
     const log = `${Date.now()} : New Req received \n`;
     fs.appendFile("log.txt" , log , (err ,dat) => {
         res.end(`Your joining time is ${Date.now()} `);
